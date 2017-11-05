a00e15cf5c16a934d9d447bf3c97b16fc6c4fb04
Anant <anantkgoyal@gmail.com>
2017-11-05 09:11:35 -0600

    Checking to see if the input is a valid URL

--- PageExplorer/src/Navigator.java
+++ PageExplorer/src/Navigator.java
@@ -1,8 +1,21 @@
+import java.lang.Object;
+import java.net.*;
 
 public class Navigator 
 {
 	public static String Naviagte(String input)
 	{
+		URL inputPage;
+		try 
+		{
+			inputPage = new URL(input);
+		} catch (MalformedURLException e) 
+		{
+			return "Invalid URL";
+		}
+		
+		
+		
 		System.out.println(input);
 		return "hello World";
 	}
